690118b33857a0bd3f81baa9784c16bfcda883be
Julian <julianb2703@gmail.com>
2020-05-17 14:40:23 +0200

    Remindersorter fixxed

--- TimePlannerUpdated/ReminderSorter.cs
+++ TimePlannerUpdated/ReminderSorter.cs
@@ -6,32 +6,43 @@ namespace TimePlannerUpdated
     {
         public int Compare(Reminder x, Reminder y)
         {
-            // This needs work
-            int ret = -1;
+            // -1 x is first in list
 
-            if (x.Deadline == null && y.Deadline == null)
+            int ret = 0;
+            if (x.Done == y.Done && x.Deadline.Equals(y.Deadline))
             {
                 ret = 0;
             }
-            if (x.Deadline == null)
+            else if (x.Done && !y.Done)
             {
                 ret = 1;
             }
-            else if (y.Deadline == null)
+            else if (!x.Done && y.Done)
             {
                 ret = -1;
+            }
+            else if (x.Deadline == null || y.Deadline == null)
+            {
+                if (x.Deadline != null)
+                {
+                    ret = 1;
+                }
+                else if (y.Deadline != null)
+                {
+                    ret = -1;
+                }
+                else
+                {
+                    ret = 0;
+                }
             }
             else if (x.Deadline < y.Deadline)
             {
                 ret = -1;
             }
-            else if (x.Deadline > y.Deadline)
+            else if (y.Deadline < x.Deadline)
             {
                 ret = 1;
-            }
-            else if (x.Deadline == y.Deadline)
-            {
-                ret = 0;
             }
             return ret;
         }
