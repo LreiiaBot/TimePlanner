15df318fd057dadcd88e3e03cbb2a5da1ed443c1
Julian <julianb2703@gmail.com>
2020-07-26 10:32:58 +0200

    changed folder structure

---
+++ TimePlannerUpdated.Default/Model/Exception/PlanningExcpetion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimePlannerUpdated.Default.Model
+{
+    class PlanningExcpetion : Exception
+    {
+        public PlanningExcpetion(string message) : base(message)
+        {
+
+        }
+    }
+}
