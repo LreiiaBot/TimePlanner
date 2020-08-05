e37d7fb1e8d7f6dc2e14789e7ba9bc8da47f7046
Julian <julianb2703@gmail.com>
2020-08-05 20:22:19 +0200

    Page for autoAddValeus added

---
+++ TimePlannerUpdated.WPFGUI/Pages/AutoAddPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TimePlannerUpdated.WPFGUI.Pages
+{
+    /// <summary>
+    /// Interaktionslogik für AutoAddPage.xaml
+    /// </summary>
+    public partial class AutoAddPage : Page
+    {
+        public AutoAddPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
